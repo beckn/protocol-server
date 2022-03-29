235e960f4651be74a91fdc5a6c819236709c3ec6
AyushAgrawal25 <agrawal.ayush2500@gmail.com>
2022-03-29 17:29:17 +0530

    Request cache added

--- src/models/response.cache.ts
+++ src/models/response.cache.ts
@@ -3,6 +3,7 @@ import { getDb } from "../utils/db";
 
 const responseCacheCollection = 'responseCache';
 const emptyPlaceHolder='N/A';
+const generalExpiryDays=1;
 
 export class ResponseCache{
     public static getInstance():ResponseCache{
@@ -37,11 +38,44 @@ export class ResponseCache{
     }
 
     public async cacheRequest(requestBody: any) {
-        
+        const db: Db=getDb();
+        const collection=db.collection(responseCacheCollection);
+
+        await this.createExpireIndex();
+
+        const expireDate=new Date(Date.now()+(1000*60*60*24*generalExpiryDays));
+        const result=await collection.insertOne({
+            expiresAt: expireDate,
+            transaction_id: requestBody.transaction_id,
+            request: requestBody
+        });
+
+        return result.insertedId.toString();
     }
 
     public async cacheResponse(responseBody: any) {
+        const db:Db=getDb();
+        const collecton=db.collection(responseCacheCollection);
         
+        await this.createExpireIndex();
+        
+        const requestData=await collecton.findOne({
+            transaction_id: responseBody.transaction_id
+        });
+
+        if(!requestData){
+            return;
+        }
+
+        const expireDate=new Date(Date.now()+(1000*60*60*24*generalExpiryDays));
+        
+        // TODO: insert the respnonseBody inside the responses array.
+        // TODO: set the expiration time as per the tll.
+        // const result=await collecton.updateOne({
+        //     transaction_id: responseBody.transaction_id,
+        // }, {
+        //     // $addToSet:
+        // })
     }
 
     public async check(requestBody: any){
