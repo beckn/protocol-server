2f5986f3e5aa74212784a6b139f17a3c612725f4
AyushAgrawal25 <agrawal.ayush2500@gmail.com>
2022-04-01 19:55:27 +0530

    TODO

--- src/controllers/actionHandler.ts
+++ src/controllers/actionHandler.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 
 export async function unConfigureActionHandler(req: Request, res: Response, next: NextFunction, action: string) {
+    // TODO: create proper error body.
     res.status(500).json({
         error: `Action ${action} is not configured.`,
     });
