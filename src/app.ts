6e56cf765ce65da0492aff5d344fdf1d8bf8aafd
AyushAgrawal25 <agrawal.ayush2500@gmail.com>
2022-04-11 12:19:09 +0530

    Config loaders are now part of it.

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,8 @@ import { createKeyPair } from "./utils/auth"
 
 const initializeExpress=async()=>{
     const app = Express()
+    
+    // Middleware for request body conversion to json and raw body creation.
     app.use(Express.json({
         verify: (req: Request, res: Response, buf: Buffer) => {
             res.locals={
@@ -16,13 +18,16 @@ const initializeExpress=async()=>{
         }    
     }))
 
-    // app.use('/',async (req:Request, res: Response, next: NextFunction) => {
-    //     logger.info(JSON.stringify(req.body));
-    //     next();
-    // })
+    // Request Logger.
+    app.use('/',async (req:Request, res: Response, next: NextFunction) => {
+        logger.info(JSON.stringify(req.body));
+        next();
+    })
 
+    // Routing.
     app.use('/', router)
 
+    // Error Handler.
     app.use((err : any, req : Request, res : Response, next : NextFunction) => {
         logger.error(err);
         res.status(err.status || 500).json({
