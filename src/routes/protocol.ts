c2b16618f0865fc0cbb5a7c235e33518074a16d9
AyushAgrawal25 <agrawal.ayush2500@gmail.com>
2022-04-11 16:34:42 +0530

    Cache added to the utils

--- src/routes/protocol.ts
+++ src/routes/protocol.ts
@@ -14,43 +14,43 @@ import { ResponseCache } from "../utils/response.cache";
 
 const router = Router()
 
-const configuredActions=getProvidedActions();
-Object.keys(ActionTypes).forEach((action)=>{
-    let isActionConfigured=false;
-    configuredActions.forEach((configuredAction)=>{
-        if(action==configuredAction){
-            isActionConfigured=true;
+const configuredActions = getProvidedActions();
+Object.keys(ActionTypes).forEach((action) => {
+    let isActionConfigured = false;
+    configuredActions.forEach((configuredAction) => {
+        if (action == configuredAction) {
+            isActionConfigured = true;
         }
     })
 
-    if(isActionConfigured){
-        if(process.env.mode=='bap'){
+    if (isActionConfigured) {
+        if (process.env.mode == 'bap') {
             // BAP Trigger
             router.post(`/${action}`, jsonConverter, authCreator, async (req: Request, res: Response, next: NextFunction) => {
                 await contextMiddleware(req, res, next, action);
             }, validator, async (req: Request, res: Response, next: NextFunction) => {
                 await triggerHandler(req, res, next, action);
             });
-            
+
             router.post(`/on_${action}`, jsonConverter, validator, auth, async (req: Request, res: Response, next: NextFunction) => {
                 await bapProtocolHandler(req, res, next, action);
             });
         }
-        else if(process.env.mode=='bpp'){    
-            router.post(`/${action}`, jsonConverter,validator, auth, async (req: Request, res: Response, next: NextFunction) => {
+        else if (process.env.mode == 'bpp') {
+            router.post(`/${action}`, jsonConverter, validator, auth, async (req: Request, res: Response, next: NextFunction) => {
                 await bppProtocolHandler(req, res, next, action);
             })
-        
+
             router.post(`/on_${action}`, jsonConverter, authCreator, validator, async (req: Request, res: Response, next: NextFunction) => {
                 await publishResults(req, res, next, `on_${action}`);
             })
         }
     }
-    else{
+    else {
         router.post(`/${action}`, (req: Request, res: Response, next: NextFunction) => {
             unConfigureActionHandler(req, res, next, action);
         });
-            
+
         router.post(`/on_${action}`, (req: Request, res: Response, next: NextFunction) => {
             unConfigureActionHandler(req, res, next, `/on_${action}`);
         });
@@ -59,10 +59,10 @@ Object.keys(ActionTypes).forEach((action)=>{
 
 router.delete('/lookupCacbe', async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const lookupCache=LookupCache.getInstance();
+        const lookupCache = LookupCache.getInstance();
         await lookupCache.clear();
         res.status(200).json({
-            result:"Success",
+            result: "Success",
             message: "Lookup Cache Cleared"
         });
     } catch (error) {
@@ -71,10 +71,10 @@ router.delete('/lookupCacbe', async (req: Request, res: Response, next: NextFunc
 });
 router.delete('/responseCacbe', async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const responseCache=ResponseCache.getInstance();
+        const responseCache = ResponseCache.getInstance();
         await responseCache.clear();
         res.status(200).json({
-            result:"Success",
+            result: "Success",
             message: "Response Cache Cleared"
         });
     } catch (error) {
