b2ceb320763102b65d405dff25429a89772f31c2
AyushAgrawal25 <agrawal.ayush2500@gmail.com>
2022-04-12 11:31:30 +0530

    Minor change on client callback

--- src/utils/callbacks.ts
+++ src/utils/callbacks.ts
@@ -2,8 +2,15 @@ import axios from "axios";
 
 export async function clientCallback(requestBody: any, isError: boolean) {
     try {
-        if((isError)&&(requestBody.error==null)){
-            throw new Error('Error object is missing.');
+        if(isError){
+            if(!requestBody.error){
+                throw new Error('Error object is missing.');
+            }
+        }
+        else{
+            if(requestBody.error){
+                throw new Error("Error object is present even if it's not error callback.");
+            }
         }
 
         await axios.post(process.env.clientUrl!, requestBody);
