72e1c7755a4b4383bcaa6125e3ec4535c4e57043
Zoya <zoya@eminds.ai>
2023-08-18 15:55:57 +0530

    Context_builder_update

--- src/middlewares/context.middleware.ts
+++ src/middlewares/context.middleware.ts
@@ -2,18 +2,19 @@ import { NextFunction, Request, Response } from "express";
 import { Locals } from "../interfaces/locals.interface";
 import { AppMode } from "../schemas/configs/app.config.schema";
 import { getConfig } from "../utils/config.utils";
-import { bapContextBuilder, bppContextBuilder } from "../utils/context.utils";
+import { contextBuilder } from "../utils/context.utils";
 
 export async function contextBuilderMiddleware(req: Request, res: Response<{}, Locals>, next: NextFunction, action: string) {
     try {
         let context=null;
         switch(getConfig().app.mode){
             case AppMode.bap:{
-                context=await bapContextBuilder(req.body.context, action);
+                context=await contextBuilder(req.body.context, action);
+               
                 break;
             }
             case AppMode.bpp:{
-                context=bppContextBuilder(req.body.context, action);
+                context=await contextBuilder(req.body.context, action);
                 break;
             }
         }
