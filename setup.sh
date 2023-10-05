b22cbf0ed531a9d584fffc37a0fb25404f6b188a
prasad-takale-eminds <84436945+prasad-takale-eminds@users.noreply.github.com>
2023-10-05 15:43:45 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 cp devops/deploy-bap.sh ~/
 cp devops/deploy-bpp.sh ~/
 cp devops/Dockerfile ~/
-cp config/samples/bap-client.yaml ~/dfault-bap-client.yml
-cp config/samples/bap-network.yaml ~/dfault-bap-network.yml
-cp config/samples/bpp-client.yaml ~/dfault-bpp-client.yml
-cp config/samples/bpp-network.yaml ~/dfault-bpp-network.yml
-mkdir docker_data && cp docker/docker-compose.yaml ~/docker_data
+cp config/samples/bap-client.yaml ~/default-bap-client.yml
+cp config/samples/bap-network.yaml ~/default-bap-network.yml
+cp config/samples/bpp-client.yaml ~/default-bpp-client.yml
+cp config/samples/bpp-network.yaml ~/default-bpp-network.yml
+mkdir ~/docker_data && cp docker/docker-compose.yaml ~/docker_data/
