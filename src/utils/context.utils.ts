aa2429e107c34380fd8d4e64b2752224a951e848
ShivaniDhoke <shivani.dhoke@eminds.ai>
2024-01-02 19:21:18 +0530

    fixed the issue

--- src/utils/context.utils.ts
+++ src/utils/context.utils.ts
@@ -48,11 +48,11 @@ export const bapContextBuilder = async (
       ttl: moment.duration(getConfig().app.ttl, "ms").toISOString(),
       action: ActionUtils.parseAction(context.action),
       timestamp: new Date().toISOString(),
-      message_id: uuid_v4(),
+      message_id: context?.message_id ? context?.message_id : uuid_v4(),
       key: context?.key,
       transaction_id: context.transaction_id
         ? context.transaction_id
-        : uuid_v4()
+        : uuid_v4(),
     }
   );
   logger.info(`BAP Context:\n ${JSON.stringify(bapContext)}\n\n`);
@@ -114,7 +114,7 @@ export const bppContextBuilder = (context: any, action: string): any => {
     message_id: context.message_id,
 
     ttl: moment.duration(getConfig().app.ttl, "ms").toISOString(),
-    timestamp: new Date().toISOString()
+    timestamp: new Date().toISOString(),
   };
   return bppContext;
 };
