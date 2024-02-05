38b57984e9a364829b1c704fddc6cfa460abf23c
shreyvishal <shreya.vishal@eminds.ai>
2024-02-05 16:07:25 +0530

    Added:Cofigurable Logs

--- src/utils/logger.utils.ts
+++ src/utils/logger.utils.ts
@@ -1,56 +1,68 @@
-import * as winston from "winston"
-import "winston-daily-rotate-file"
+import * as winston from "winston";
+import "winston-daily-rotate-file";
+import { getLogsConfig } from "./env.utils";
 
-const myFormat = winston.format.printf(({ level, message, label, timestamp }) => {
-  return `${timestamp} [${label}] ${level}: ${message}`;
-});
+const myFormat = winston.format.printf(
+  ({ level, message, label, timestamp }) => {
+    return `${timestamp} [${label}] ${level}: ${message}`;
+  }
+);
 
 const logger = winston.createLogger({
-    levels: winston.config.syslog.levels,
-    format: winston.format.combine(winston.format.label({ label: "test" }), winston.format.timestamp(), myFormat),
-    transports: [
-        new winston.transports.Console(),
-        new winston.transports.DailyRotateFile({
-            level: "info",
-            filename: 'logs/info/%DATE%.log',
-            datePattern: 'YYYY-MM-DD-HH',
-            zippedArchive: true,
-            maxSize: '20m',
-            maxFiles: '30d'
-        }),
-        new winston.transports.DailyRotateFile({
-            level: "error",
-            filename: 'logs/error/%DATE%.log',
-            datePattern: 'YYYY-MM-DD-HH',
-            zippedArchive: true,
-            maxSize: '20m',
-            maxFiles: '30d'
-        }),
-        new winston.transports.DailyRotateFile({
-            level: "http",
-            filename: 'logs/http/%DATE%.log',
-            datePattern: 'YYYY-MM-DD-HH',
-            zippedArchive: true,
-            maxSize: '20m',
-            maxFiles: '30d'
-        }),
-        new winston.transports.DailyRotateFile({
-            level: "warn",
-            filename: 'logs/warn/%DATE%.log',
-            datePattern: 'YYYY-MM-DD-HH',
-            zippedArchive: true,
-            maxSize: '20m',
-            maxFiles: '30d'
-        }),
-        new winston.transports.DailyRotateFile({
-            level: "debug",
-            filename: 'logs/debug/%DATE%.log',
-            datePattern: 'YYYY-MM-DD-HH',
-            zippedArchive: true,
-            maxSize: '20m',
-            maxFiles: '30d'
-        }),
-    ]
-})
+  levels: winston.config.syslog.levels,
+  format: winston.format.combine(
+    winston.format.label({ label: "test" }),
+    winston.format.timestamp(),
+    myFormat
+  ),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.DailyRotateFile({
+      level: "info",
+      filename: "logs/info/%DATE%.log",
+      datePattern: "YYYY-MM-DD-HH",
+      zippedArchive: true,
+      maxSize: "20m",
+      maxFiles: "30d",
+      silent: !getLogsConfig().includes("info")
+    }),
+    new winston.transports.DailyRotateFile({
+      level: "error",
+      filename: "logs/error/%DATE%.log",
+      datePattern: "YYYY-MM-DD-HH",
+      zippedArchive: true,
+      maxSize: "20m",
+      maxFiles: "30d",
+      silent: !getLogsConfig().includes("error")
+    }),
+    new winston.transports.DailyRotateFile({
+      level: "http",
+      filename: "logs/http/%DATE%.log",
+      datePattern: "YYYY-MM-DD-HH",
+      zippedArchive: true,
+      maxSize: "20m",
+      maxFiles: "30d",
+      silent: !getLogsConfig().includes("http")
+    }),
+    new winston.transports.DailyRotateFile({
+      level: "warn",
+      filename: "logs/warn/%DATE%.log",
+      datePattern: "YYYY-MM-DD-HH",
+      zippedArchive: true,
+      maxSize: "20m",
+      maxFiles: "30d",
+      silent: !getLogsConfig().includes("warn")
+    }),
+    new winston.transports.DailyRotateFile({
+      level: "debug",
+      filename: "logs/debug/%DATE%.log",
+      datePattern: "YYYY-MM-DD-HH",
+      zippedArchive: true,
+      maxSize: "20m",
+      maxFiles: "30d",
+      silent: !getLogsConfig().includes("debug")
+    })
+  ]
+});
 
 export default logger;
