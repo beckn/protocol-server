9e05ffe98d75641967c313d806d097567419d9a4
shiva-rakshith <rakshiths@sanketika.in>
2024-05-22 12:44:24 +0530

    feat: clean up repo and update sample config files

--- devops/deploy-bap.sh
+++ devops/deploy-bap.sh
@@ -1,8 +1,8 @@
-cp $HOME/Dockerfile $HOME/default-bap-client.yml $HOME/default-bap-network.yml $HOME/protocol-server
-cd $HOME/protocol-server
+cp ~/Dockerfile ~/default-bap-client.yml ~/default-bap-network.yml ~/protocol-server
+cd ~/protocol-server
 
-sudo sudo docker build -t bap-client --build-arg default_yml=default-bap-client.yml --build-arg port=BAP_CLIENT_PORT .
-sudo sudo docker build -t bap-network --build-arg default_yml=default-bap-network.yml --build-arg port=BAP_NETWORK_PORT .
+sudo sudo docker build -t bap-client --build-arg default_yml=default-bap-client.yml --build-arg port=5001 .
+sudo sudo docker build -t bap-network --build-arg default_yml=default-bap-network.yml --build-arg port=5002 .
 
 sudo docker stop bap-network bap-client
 sudo docker rm bap-network bap-client
--- devops/deploy-bpp.sh
+++ devops/deploy-bpp.sh
@@ -1,8 +1,8 @@
-cp $HOME/Dockerfile $HOME/default-bpp-client.yml $HOME/default-bpp-network.yml $HOME/protocol-server
-cd $HOME/protocol-server
+cp ~/Dockerfile ~/default-bpp-client.yml ~/default-bpp-network.yml ~/protocol-server
+cd ~/protocol-server
 
-sudo docker build -t bpp-client --build-arg default_yml=default-bpp-client.yml --build-arg port=BPP_CLIENT_PORT .
-sudo docker build -t bpp-network --build-arg default_yml=default-bpp-network.yml --build-arg port=BPP_NETWORK_PORT .
+sudo docker build -t bpp-client --build-arg default_yml=default-bpp-client.yml --build-arg port=6001 .
+sudo docker build -t bpp-network --build-arg default_yml=default-bpp-network.yml --build-arg port=6002 .
 
 sudo docker stop bpp-network bpp-client
 sudo docker rm bpp-network bpp-client
