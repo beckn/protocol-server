01b7d0ca6765a97254ca63031ea1d61f5d26b43b
shreyvishal <shreya.vishal@eminds.ai>
2024-06-18 16:29:13 +0530

    Added: Feature For load OpenAPIValidator Cache on app boot

--- src/middlewares/schemaValidator.middleware.ts
+++ src/middlewares/schemaValidator.middleware.ts
@@ -173,7 +173,7 @@ const initializeOpenApiValidatorCache = async (
       actions = Object.keys(ResponseActions);
     }
 
-    actions.slice(0, 2).forEach((action) => {
+    actions.forEach((action) => {
       const mockRequest = (body: any) => {
         const req = httpMocks.createRequest({
           method: "POST",
